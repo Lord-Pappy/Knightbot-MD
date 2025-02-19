72d9e7b3360b19214b5b608d64b6064c2665a27a
Lord-Pappy <obengenoch809@gmail.com>
2025-02-19 07:13:20 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "LordPappy Bot",
+  botOwner: 'LordPappy Bot', // Your name
+  ownerNumber: '0257586236', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
